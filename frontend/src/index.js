931ac224da1f3fb1de143880eb1f76a7cb67a0f7
Abdel-Rahman-Emam <77146272+Abdel-Rahman-Emam@users.noreply.github.com>
2021-09-22 20:34:58 +0200

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {AuthContextProvider} from "./context/AuthContext"
+import {QueryClient, QueryClientProvider} from "react-query"
+
+const queryClient = new QueryClient();
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <QueryClientProvider client = {queryClient}>
+      <App />
+      </QueryClientProvider>
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
